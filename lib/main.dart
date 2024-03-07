6283edb2feb54c6bfe4c96ca8d00f2475b91b953
yafizh <a.chairil.yafizh@gmail.com>
2024-03-07 20:49:14 +0700

    firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:brew_crew/screens/wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
