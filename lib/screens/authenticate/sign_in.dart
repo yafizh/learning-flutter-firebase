57864200f23a376c07e01d78aa2f3d50aeeaef27
yafizh <a.chairil.yafizh@gmail.com>
2024-03-07 20:38:22 +0700

    basic app structure

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Sign in to Brew Crew'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: ElevatedButton(
+          child: Text('Sign in anon'),
+          onPressed: () async {},
+        ),
+      ),
+    );
+  }
+}
