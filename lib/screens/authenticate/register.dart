f1ec39a65f1c99c9719595565a3c6b15c19fbcac
yafizh <a.chairil.yafizh@gmail.com>
2024-03-09 20:18:01 +0700

    register with email & password

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
@@ -12,13 +13,16 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
@@ -45,6 +49,7 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               SizedBox(height: 20.0),
@@ -54,6 +59,8 @@ class _RegisterState extends State<Register> {
                     email = val;
                   });
                 },
+                validator: (val) =>
+                    (val == null || val.isEmpty) ? 'Enter an email' : null,
               ),
               SizedBox(height: 20.0),
               TextFormField(
@@ -63,6 +70,9 @@ class _RegisterState extends State<Register> {
                     password = val;
                   });
                 },
+                validator: (val) => (val == null || val.length < 6)
+                    ? 'Enter a password 6+ chars long'
+                    : null,
               ),
               SizedBox(height: 20.0),
               ElevatedButton(
@@ -74,13 +84,25 @@ class _RegisterState extends State<Register> {
                   ),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState!.validate()) {
+                    dynamic result = await _auth.registerWithEmailAndPassword(
+                        email, password);
+                    if (result == null) {
+                      setState(() {
+                        error = 'please supply a valid email';
+                      });
+                    }
+                  }
                 },
                 child: Text(
                   "Register",
                   style: TextStyle(color: Colors.white),
                 ),
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
               )
             ],
           ),
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -27,6 +27,19 @@ class AuthService {
     }
   }
 
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
   Future signOut() async {
     try {
       return await _auth.signOut();
